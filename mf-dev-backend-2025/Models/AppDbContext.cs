e85bd15efc72d9a8dfa9e67e0a34779828a6ae6d
michaloumen <michelle.lmendonca@gmail.com>
2025-04-12 18:34:25 -0300

    configuration for database

---
+++ mf-dev-backend-2025/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2025.Models
+{
+    public class AppDbContext : DbContext
+        // estou criando configurações do banco de dados
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veículo> Veículos { get; set; }
+    }
+}
