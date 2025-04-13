76c6135163cdd208371327b89653c32f48c568c5
michaloumen <michelle.lmendonca@gmail.com>
2025-04-12 21:44:37 -0300

    cria e lista veículos

--- mf-dev-backend-2025/Controllers/VeículosController.cs
+++ mf-dev-backend-2025/Controllers/VeículosController.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace mf_dev_backend_2025.Controllers
 {
-    public class VeículosController: Controller
+    public class VeículosController : Controller
     {
         private readonly AppDbContext _context;
         public VeículosController(AppDbContext context)
@@ -13,9 +13,30 @@ namespace mf_dev_backend_2025.Controllers
         }
 
         // cada função aqui no controller é uma página
-        public async Task<IActionResult> Index() { 
+        public async Task<IActionResult> Index()
+        {
             var dados = await _context.Veículos.ToListAsync();
-            return View(dados); 
+            return View(dados);
+        }
+
+        // get de veículos
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // post de veículos
+        [HttpPost]
+        public async Task <IActionResult> Create(Veículo veículo)
+        {
+            if (ModelState.IsValid) 
+            {
+                _context.Veículos.Add(veículo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(veículo);
         }
     }
 }
