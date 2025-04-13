3ba5a2ca67022660acdf6f2fcac06848b8381a14
michaloumen <michelle.lmendonca@gmail.com>
2025-04-12 21:17:58 -0300

    Criando controller e view

---
+++ mf-dev-backend-2025/Controllers/VeículosController.cs
@@ -0,0 +1,21 @@
+﻿using mf_dev_backend_2025.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2025.Controllers
+{
+    public class VeículosController: Controller
+    {
+        private readonly AppDbContext _context;
+        public VeículosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // cada função aqui no controller é uma página
+        public async Task<IActionResult> Index() { 
+            var dados = await _context.Veículos.ToListAsync();
+            return View(dados); 
+        }
+    }
+}
